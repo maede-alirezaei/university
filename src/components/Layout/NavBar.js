ef9e21125a36d21cd5793acbc85a566b25208812
DESKTOP-RLUVES5\Maedeh Tehrani <maede.alirezaei@gmail.com>
2022-11-01 15:31:43 +0330

    adding style for navbar

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -1,26 +1,95 @@
-import React from "react";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import uniLogo from "../../assets/UniLogo.jpg";
 
-const NavBar = () => {
+const navigation = [
+  { name: "خانه", path: "/" },
+  { name: "دستاورد ها", path: "/achievements" },
+  { name: "مورد علاقه ها", path: "/favourites" },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function NavBar() {
+  const [current, setCurrent] = useState(0);
+
   return (
-    <header>
-      <div>{"دانشگاه علامه طباطبایی"}</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">{"خانه"}</Link>
-          </li>
-          <li>
-            <Link to="/achievements">{"دستاوردها"}</Link>
-          </li>
-          <li>
-            <Link to="/favourites">{"موردعلاقه ها"}</Link>
-          </li>
-        </ul>
-      </nav>
-      
-    </header>
-  );
-};
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="ml-4 flex flex-shrink-0 items-center">
+                  <img
+                    className="block h-8 w-auto lg:hidden"
+                    src={uniLogo}
+                    alt="Allameh Logo"
+                  />
+                  <img
+                    className="hidden h-8 w-auto lg:block"
+                    src={uniLogo}
+                    alt="Allameh Logo"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item, index) => (
+                      <Link
+                        key={item.name}
+                        to={item.path}
+                        className={classNames(
+                          current === index
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "px-3 py-2 rounded-md text-sm font-medium"
+                        )}
+                        onClick={() => setCurrent(index)}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
-export default NavBar;
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.path}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
